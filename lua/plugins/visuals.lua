0e89ecc7de8c90e58ed9ce7bbb28ee883e56e84b
toasted-cornkernels <volcano0909@gmail.com>
2025-02-01 23:05:50 -0800

    update

--- lua/plugins/visuals.lua
+++ lua/plugins/visuals.lua
@@ -16,20 +16,6 @@ return {
     end
   },
   {
-    "f-person/auto-dark-mode.nvim",
-    opts = {
-      update_interval = 1000,
-      set_light_mode = function()
-        vim.api.nvim_set_option_value("background", "light", {})
-        vim.cmd("colorscheme dayfox")
-      end,
-      set_dark_mode = function()
-        vim.api.nvim_set_option_value("background", "dark", {})
-        vim.cmd("colorscheme carbonfox")
-      end,
-    },
-  },
-  {
     "catgoose/nvim-colorizer.lua",
     event = "BufReadPre",
     lazy = true,
