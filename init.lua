c73e80b0a9ee27f5b6305d6189a84b4c51a140fe
Jeongsoo Lee <jeongsoolee09@github.com>
2025-02-10 17:11:25 -0800

    update

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-vim.opt.shortmess = "I"
+vim.opt.shortmess = vim.opt.shortmess + "ltToOCFI"
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
