5027b369d415ba988d35c689bbcf52701567f8ed
toasted-cornkernels <volcano0909@gmail.com>
2025-02-15 18:23:40 -0800

    update

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -5,6 +5,7 @@ return {
       local cmp = require 'cmp'
 
       cmp.setup({
+	preselect = cmp.PreselectMode.None,
         snippet = {
           expand = function(args)
             vim.fn["UltiSnips#Anon"](args.body)
@@ -15,11 +16,13 @@ return {
           documentation = cmp.config.window.bordered(),
         },
         mapping = cmp.mapping.preset.insert({
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<tab>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+	  ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-d>'] = cmp.mapping.scroll_docs(4),
+          ['<C-g>'] = cmp.mapping.abort(),
+          ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+          ['<CR>'] = cmp.config.disable
         }),
         sources = cmp.config.sources({
           { name = 'nvim_lsp' },
